9e33759ac64ddf8430e7725511ad6f3d0d2cb1f8
Mattias Soderberg <mattiassoderberj@gmail.com>
2020-12-27 15:16:33 +0100

    added a bunch of exersize code from PCC

---
+++ full_name.py
@@ -0,0 +1,5 @@
+name = "kal"
+last_name = "lovelace"
+full_name = f"{name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
---
+++ name_cases.py
@@ -0,0 +1,9 @@
+name = "Nina"
+print(f"Hello, {name}! Would you like to learn some python?")
+name_2 = "nina pentti"
+print(name_2)
+print(name_2.upper())
+print(name_2.title())
+quote = '"A person who never made a mistake, never tried anything new"'
+fam_person = " Albert Einstein "
+print(f"A wise person once said, {quote}\n\t-{fam_person.strip()}!")
